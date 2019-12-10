1bf66aecba5dcb6b960e6918f1012c4e50459529
Margaret <match14@yandex.ru>
2019-12-11 02:54:52 +0300

    страница

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,15 @@
 $(document).ready(function () {
+	// Маска для телефона
+	$.mask.definitions['~'] = "[+-]";
+	$(".js-phone").mask("+7 (999) 999-9999");
 
-	
-	// //---------- Маска для телефона -------------
-	// $.mask.definitions['~'] = "[+-]";
-	// $("#phone").mask("(999) 999-9999");
-
-	// // Вызов функции подгрузки изображений
-	// loadBigImg();
-	// loadBigBacground();
-
-	// // Вызов функции прижатия футера к низу экрана
-	// footerBind('.js-main','.js-header,.js-footer');
-	// $(window).on('resize',function(){footerBind('.js-main','.js-header,.js-footer')});
+	// Стилизация выпадающего списка
+	if ($('.js-select').length) {
+		$('.js-select').select2({
+			minimumResultsForSearch: Infinity,
+			placeholder: function(){
+				$(this).attr('data-placeholder');
+			},
+		});
+	}
 });
-
-// // Загрузка больших изображений
-// function loadBigImg() {
-// 	var $imgDefer = $('[data-src]');
-
-// 	$imgDefer.each(function(indx, element){
-// 		var urlImgBig = $(this).attr("data-src");
-// 		$(this).attr("src", urlImgBig);
-// 	});
-// }
-
-// function loadBigBacground() {
-// 	var $imgDefer = $('[data-background]');
-
-// 	$imgDefer.each(function(indx, element){
-// 		var urlBackgroundBig = $(this).attr("data-background");
-// 		$(this).css("background-image", "url("+ urlBackgroundBig +")");
-// 	});
-// }
